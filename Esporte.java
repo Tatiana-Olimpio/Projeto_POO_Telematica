c07c3ad9e475ca35878d852f757c3b6c287cfa01
Tatiana-Olimpio <31863831+Tatiana-Olimpio@users.noreply.github.com>
2017-09-21 18:33:50 -0300

    Update Esporte.java

--- Esporte.java
+++ Esporte.java
@@ -1,29 +1,52 @@
 //Autor: Tatiana Olímpio
 import java.util.Scanner;
 
+
 //Classe Esporte
 
-public class Esporte {
-	//Atributos
+class Esporte extends Evento{
+	public Esporte(String nome, String descricao, String dataEvento,
+			String horaEvento, String sinopse, String elenco) {
+		super(nome, descricao, dataEvento, horaEvento, sinopse, elenco);
 	
+	}
+
+	//Atributos
 	private String nomeDoEsporte;
 	private String equipe;
 	
 	Scanner entrada = new Scanner(System.in);
 		
 // construtor
-	public Esporte( String nomeDoEsporte, String equipe){
-		this.nomeDoEsporte = nomeDoEsporte;
-		this.equipe= equipe;
-	}
+	//public Esporte( String nomeDoEsporte, String equipe){
+	//	this.nomeDoEsporte = nomeDoEsporte;
+		//this.equipe= equipe;
+	//}
 
-//  dados do esporte
-		String getDados(){
-			return "\n\nTipo de esporte:\n\nNome do esporte: " + this.nomeDoEsporte + "\nEquipe: " + this.equipe;
-		}	
+
 
 	//Metodos
 	
+	public String getNomeDoEsporte() {
+			return nomeDoEsporte;
+		}
+
+		public void setNomeDoEsporte(String nomeDoEsporte) {
+			this.nomeDoEsporte = nomeDoEsporte;
+		}
+
+		public String getEquipe() {
+			return equipe;
+		}
+
+		public void setEquipe(String equipe) {
+			this.equipe = equipe;
+		}
+
+		@Override
+		public String toString() {
+			return "Nome do esporte: " + this.nomeDoEsporte + ", Equipe: " + this.equipe;
+		}
 	void registrarGinasio(){
 		
 	}
