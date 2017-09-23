fc47c5da7c67a4a251705c60874716210fc4e69c
Tatiana-Olimpio <31863831+Tatiana-Olimpio@users.noreply.github.com>
2017-09-22 23:25:42 -0300

    Add files via upload

--- projeto.java
+++ projeto.java
@@ -9,16 +9,19 @@ public class projeto{
 	public static void main(String[] args) {
 		ArrayList<Cliente> clientes = new ArrayList<>();
 		ArrayList<Evento> eventos = new ArrayList<>();
-		//ArrayList<Ambientes> ambientes = new ArrayList<>();
+		ArrayList<Ambiente> ambientes = new ArrayList<>();
 		
   		Cliente c1 = new Cliente( "098.097.9876-78", "fulano@gmailcom", 
 				 "Rua Sergipe, São Luiz", 2218764, 98765, "Fulano_alburqueque", 45678);
 		clientes.add(c1);
 		
-		//Ambiente a = new Ambiente("Espaço de show", 1000 ,"Rua: Palmeiras","Recanto do lazer","Proximo ao MACRO supermercado",600);
-		//ambientes.add(c1);
+		Ambiente a1 = new Estadio("Espaço de show",1000,"Rua: Palmeiras","Recanto do lazer","Proximo ao MACRO supermercado",600);
+		ambientes.add(a1);
+		for (Iterator<Ambiente> iterator = ambientes.iterator(); iterator.hasNext();) {
+			System.out.println(iterator.toString());
+			
+		}
 		
-		//Esporte e = new Esporte("Futsal" , "Maraisa, Monalisa, Jessyka, Carol");
 		
 		Evento ev = new Esporte("Futsal", "Jogo com 4 pessoas", "13/10/2017", "14hrs", "Maraisa, Monalisa, Jessyka, Carol", null);
 		eventos.add(ev);
@@ -29,13 +32,6 @@ public class projeto{
 		}
 			
 		}
-		//Filme f = new Filme("Invasão Zumbi", 16, "Terro, zumbi");
 		
-		//verificar dados:
-		//System.out.println(c.getDados());
-		//System.out.println(a.getDados());
-		//System.out.println(e.getDados());
-		//System.out.println(ev.getDados());
-		//System.out.println(f.getDados());
 	}
 	 
